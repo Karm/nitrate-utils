27992d8d4d07e55d2f58ace230c9420505fe8572
Michal Karm Babacek <mbabacek@redhat.com>
2013-01-02 17:59:02 +0100

    check.py goes for children

--- check.py
+++ check.py
@@ -5,6 +5,14 @@
 import re, sys, optparse
 from nitrate import *
 
+def countRuns(testruns):
+    matching_runs_counter = 0
+    for testrun in testruns:
+        if str(testrun.build) == str(options.build):
+            matching_runs_counter = matching_runs_counter + 1
+    return matching_runs_counter
+
+
 if __name__ == "__main__":
     parser = optparse.OptionParser(usage="check.py --plan PLAN --build BUILD [options]")
     parser.add_option("--plan", dest="plan", type="int", help="test plan id")
@@ -13,14 +21,21 @@ if __name__ == "__main__":
 
     testplan = TestPlan(options.plan)
 
-overall_counter = 0
-for testplan in TestPlan.search(parent=testplan.id):
-    print testplan, testplan.status
-    matching_runs_counter = 0
-    for testrun in testplan.testruns:
-        if str(testrun.build) == str(options.build):
-            matching_runs_counter = matching_runs_counter + 1
-    print "Runs created for build %s: %d" % (options.build, matching_runs_counter)
-    overall_counter = overall_counter + matching_runs_counter
+    overall_counter = 0
 
-print "For all the test plans, there are %d runs with build %s" % (overall_counter, options.build)
+    print color("Warning: This script may take dozens of minutes to complete :-(", color="lightred", background="black")
+  
+    for testplan in TestPlan.search(parent=testplan.id):
+        print "[PLAN] %s %s" % (testplan, testplan.status)
+        runs_count = countRuns(testplan.testruns)
+        overall_counter = overall_counter + runs_count
+        print "    Runs created for build %s: %d" % (options.build, runs_count)
+        for child in testplan.children:
+            print "    [CHILD plan] %s %s" % (child, child.status)
+            runs_count = countRuns(child.testruns)
+            overall_counter = overall_counter + runs_count
+            print "        Runs created for build %s: %d" % (options.build, runs_count)
+    
+    print "For all the test plans, there are %d runs with build %s" % (overall_counter, options.build)
+
+

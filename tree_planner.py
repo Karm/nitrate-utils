fb52851146b1745973e671c56f070ad6947d0ac9
Michal Karm Babacek <mbabacek@redhat.com>
2013-01-02 15:37:45 +0100

    tree_planner for adding particular Test plans to the defined parent.

---
+++ tree_planner.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+# @author Michal Karm Babacek
+
+import re, sys, optparse
+from nitrate import *
+
+if __name__ == "__main__":
+    parser = optparse.OptionParser(usage="check.py --plan PLAN --build BUILD [options]")
+    parser.add_option("--plan", dest="plan", type="int", help="test plan id")
+    parser.add_option("--build", dest="build", type="int", help="build id")
+    options = parser.parse_args()[0]
+
+our_testplans = {}
+new_parent_plan = TestPlan(options.plan)
+
+for testrun in TestRun.search(build=options.build):
+    our_testplans[testrun.testplan.id] = testrun.testplan
+print "--------------------------------TEST PLANS---------------------------------"
+for key in our_testplans.keys():
+    print "Test plan ID: %d | Test plan name: %s" % (key, our_testplans[key])
+    print "    Parent: %s" % our_testplans[key].parent
+    if our_testplans[key].parent is not None:
+        print "        Grandparent: %s" % our_testplans[key].parent.parent
+    else:
+        print "    Setting Parent to: %s" % new_parent_plan
+        our_testplans[key].parent = new_parent_plan

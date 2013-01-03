c87eb28da2c097c1d36695f1ebc5f03198bbdcb2
Michal Karm Babacek <mbabacek@redhat.com>
2013-01-03 12:38:51 +0100

    More exception handling on lazy load...

--- check.py
+++ check.py
@@ -6,6 +6,15 @@ import re, sys, optparse, time
 from nitrate import *
 import xmlrpclib
 
+def logerror(err):
+    print color("ProtocolError, we will try it again in 5 seconds.", color="lightred", background="black")
+    print "    A protocol error occurred"
+    print "    URL: %s" % err.url
+    print "    HTTP/HTTPS headers: %s" % err.headers
+    print "    Error code: %d" % err.errcode
+    print "    Error message: %s" % err.errmsg
+    time.sleep(5)
+
 def countRuns(testruns):
     matching_runs_counter = 0
     for testrun in testruns:
@@ -13,13 +22,7 @@ def countRuns(testruns):
             if str(testrun.build) == str(options.build):
                 matching_runs_counter = matching_runs_counter + 1
         except xmlrpclib.ProtocolError, err:
-            print color("ProtocolError, we will try it again in 5 seconds.", color="lightred", background="black")
-            print "    A protocol error occurred"
-            print "    URL: %s" % err.url
-            print "    HTTP/HTTPS headers: %s" % err.headers
-            print "    Error code: %d" % err.errcode
-            print "    Error message: %s" % err.errmsg
-            time.sleep(5)
+            logerror(err)
             if str(testrun.build) == str(options.build):
                 matching_runs_counter = matching_runs_counter + 1
     return matching_runs_counter
@@ -38,15 +41,35 @@ if __name__ == "__main__":
   
     for testplan in TestPlan.search(parent=testplan.id):
         print "[PLAN] %s %s" % (testplan, testplan.status)
-        runs_count = countRuns(testplan.testruns)
+
+        try:
+            testplan_testruns = testplan.testruns
+        except xmlrpclib.ProtocolError, err:
+            logerror(err)
+            testplan_testruns = testplan.testruns
+
+        runs_count = countRuns(testplan_testruns)
+        
         overall_counter = overall_counter + runs_count
         print "    Runs created for build %s: %d" % (options.build, runs_count)
-        for child in testplan.children:
+
+        try:
+            testplan_children = testplan.children
+        except xmlrpclib.ProtocolError, err:
+            logerror(err)
+            testplan_children = testplan.children
+
+        for child in testplan_children:
             print "    [CHILD plan] %s %s" % (child, child.status)
-            runs_count = countRuns(child.testruns)
+
+            try:
+                child_testruns = child.testruns
+            except xmlrpclib.ProtocolError, err:
+                logerror(err)
+                child_testruns = child.testruns
+
+            runs_count = countRuns(child_testruns)
             overall_counter = overall_counter + runs_count
             print "        Runs created for build %s: %d" % (options.build, runs_count)
     
     print "For all the test plans, there are %d runs with build %s" % (overall_counter, options.build)
-
-
--- tree_planner.py
+++ tree_planner.py
@@ -2,8 +2,19 @@
 
 # @author Michal Karm Babacek
 
-import re, sys, optparse
+import re, sys, optparse, time
 from nitrate import *
+import xmlrpclib
+
+def logerror(err):
+
+    print color("ProtocolError, we will try it again in 5 seconds.", color="lightred", background="black")
+    print "    A protocol error occurred"
+    print "    URL: %s" % err.url
+    print "    HTTP/HTTPS headers: %s" % err.headers
+    print "    Error code: %d" % err.errcode
+    print "    Error message: %s" % err.errmsg
+    time.sleep(5)
 
 if __name__ == "__main__":
     parser = optparse.OptionParser(usage="check.py --plan PLAN --build BUILD [options]")
@@ -12,22 +23,66 @@ if __name__ == "__main__":
     options = parser.parse_args()[0]
 
 our_testplans = {}
-new_parent_plan = TestPlan(options.plan)
+try:
+    new_parent_plan = TestPlan(options.plan)
+except xmlrpclib.ProtocolError, err:
+    logerror(err)
+    new_parent_plan = TestPlan(options.plan)
 
-for testrun in TestRun.search(build=options.build):
-    our_testplans[testrun.testplan.id] = testrun.testplan
-print "--------------------------------TEST PLANS---------------------------------"
+try:
+    testruns = TestRun.search(build=options.build)
+except xmlrpclib.ProtocolError, err:
+    logerror(err)
+    testruns = TestRun.search(build=options.build)
+
+for testrun in testruns:
+    try:
+        our_testplans[testrun.testplan.id] = testrun.testplan
+    except xmlrpclib.ProtocolError, err:
+        logerror(err)
+        our_testplans[testrun.testplan.id] = testrun.testplan
+print color("--------------------------------TEST PLANS---------------------------------", color="green", background="black")
 for key in our_testplans.keys():
-    print "Test plan ID: %d | Test plan name: %s" % (key, our_testplans[key])
-    print "    Parent: %s" % our_testplans[key].parent
+    
+    try:
+        test_plan_name = our_testplans[key].name
+    except xmlrpclib.ProtocolError, err:
+        logerror(err)
+        test_plan_name = our_testplans[key].name
+    print "Test plan ID: %d | Test plan name: %s" % (key, test_plan_name)
+
+    test_plan_parent_name = our_testplans[key].parent
+    if test_plan_parent_name is not None:
+        try:
+            test_plan_parent_name = test_plan_parent_name.name
+        except xmlrpclib.ProtocolError, err:
+            logerror(err)
+            test_plan_parent_name = test_plan_parent_name.name
+    print "    Parent: %s" % test_plan_parent_name
+    
     if our_testplans[key].parent is not None:
-        print "        Grandparent: %s" % our_testplans[key].parent.parent
+    
+        test_plan_grandparent_name = our_testplans[key].parent.parent
+        if test_plan_grandparent_name is not None:
+            try:
+                test_plan_grandparent_name = test_plan_grandparent_name.name
+            except xmlrpclib.ProtocolError, err:
+                logerror(err)
+                test_plan_grandparent_name = test_plan_grandparent_name.name
+        print "        Grandparent: %s" % test_plan_grandparent_name
+    
         if our_testplans[key].parent.id != new_parent_plan.id and our_testplans[key].parent.parent is None:
-            print color("        Setting Grandparent to: %s" % new_parent_plan, color="red", background="black")
-            our_testplans[key].parent.parent = new_parent_plan
+            print color("        Setting Grandparent to: %s" % new_parent_plan, color="yellow", background="black")
+            try:
+                our_testplans[key].parent.parent = new_parent_plan
+            except xmlrpclib.ProtocolError, err:
+                logerror(err)
+                our_testplans[key].parent.parent = new_parent_plan
     else:
-        print color("    Setting Parent to: %s" % new_parent_plan, color="red", background="black")
-        our_testplans[key].parent = new_parent_plan
-
-
-
+        print color("    Setting Parent to: %s" % new_parent_plan, color="yellow", background="black")
+        try:
+            our_testplans[key].parent = new_parent_plan
+        except xmlrpclib.ProtocolError, err:
+            logerror(err)
+            our_testplans[key].parent = new_parent_plan
+print color("We are done.", color="green", background="black")

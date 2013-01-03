628eae532f835fcb7b4de01ef8f4d8ad0a7d8161
Michal Karm Babacek <mbabacek@redhat.com>
2013-01-03 11:09:48 +0100

    Handling HTTP 500 error from overloaded TCMS.

--- check.py
+++ check.py
@@ -2,16 +2,27 @@
 
 # @author Michal Karm Babacek
 
-import re, sys, optparse
+import re, sys, optparse, time
 from nitrate import *
+import xmlrpclib
 
 def countRuns(testruns):
     matching_runs_counter = 0
     for testrun in testruns:
-        if str(testrun.build) == str(options.build):
-            matching_runs_counter = matching_runs_counter + 1
+        try:
+            if str(testrun.build) == str(options.build):
+                matching_runs_counter = matching_runs_counter + 1
+        except xmlrpclib.ProtocolError, err:
+            print color("ProtocolError, we will try it again in 5 seconds.", color="lightred", background="black")
+            print "    A protocol error occurred"
+            print "    URL: %s" % err.url
+            print "    HTTP/HTTPS headers: %s" % err.headers
+            print "    Error code: %d" % err.errcode
+            print "    Error message: %s" % err.errmsg
+            time.sleep(5)
+            if str(testrun.build) == str(options.build):
+                matching_runs_counter = matching_runs_counter + 1
     return matching_runs_counter
-
 
 if __name__ == "__main__":
     parser = optparse.OptionParser(usage="check.py --plan PLAN --build BUILD [options]")

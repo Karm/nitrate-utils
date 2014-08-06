6abdaa321459e128d81e480b91fab6cb1517b089
Michal Karm Babacek <mbabacek@redhat.com>
2014-08-06 17:56:32 +0200

    cloner.py new features: --run_summary_contains  and  --preserve_caseruns

--- cloner.py
+++ cloner.py
@@ -25,22 +25,47 @@ def logDots(num, char="▒"):
         sys.stdout.write(char)
         sys.stdout.flush()
 
-def processRuns(testrun, product, new_product_version, new_build, new_tester, new_manager):
+def processRuns(testrun, product, new_product_version, new_build, new_tester, new_manager, run_summary_contains, preserve_caseruns):
     logDots(1)
-    runs.append(["OLD", str(testrun.id), str(testrun.summary), testrun.status.name, str(testrun.build)])
-    new_test_run = TestRun(id=None, testplan=testrun.testplan, build=new_build, product=product, version=new_product_version, summary=testrun.summary, notes=testrun.notes, manager=new_manager, tester=new_tester, tags=testrun.tags, testcases=testrun.testcases)
-    runs.append(["NEW", str(new_test_run.id), str(new_test_run.summary), new_test_run.status.name, str(new_test_run.build)])
+    if run_summary_contains in str(testrun.summary):
+        runs.append(["OLD", str(testrun.id), str(testrun.summary), testrun.status.name, str(testrun.build)])
+        new_test_run = TestRun(id=None, testplan=testrun.testplan, build=new_build, product=product, version=new_product_version, summary=testrun.summary, notes=testrun.notes, manager=new_manager, tester=new_tester, tags=testrun.tags, testcases=testrun.testcases)
+        runs.append(["NEW", str(new_test_run.id), str(new_test_run.summary), new_test_run.status.name, str(new_test_run.build)])
+        if preserve_caseruns:
+            testrun_caseruns = {}
+            for trial in range(0, 5):
+                try:
+                    testrun_caseruns = {}
+                    for caserun in testrun.caseruns:
+                        logDots(1,"▓")
+                        testrun_caseruns.update({caserun.testcase.id:caserun.status})
+                    break
+                except xmlrpclib.ProtocolError, err:
+                    logerror(err)
+            for trial in range(0, 5):
+                try:
+                    for new_caserun in new_test_run.caseruns:
+                        new_caserun.status = testrun_caseruns.get(new_caserun.testcase.id)
+                        new_caserun.update()
+                        logDots(1)
+                    break
+                except xmlrpclib.ProtocolError, err:
+                    logerror(err)
+    else:
+        runs.append(["SKIPPED", str(testrun.id), str(testrun.summary), testrun.status.name, str(testrun.build)])
     logDots(1,"▓")
 
 if __name__ == "__main__":
     parser = optparse.OptionParser(usage="./cloner.py --plan 7174 --build \"2.0.0.CR4\" --product \"JBoss EWS\" --new_product_version \"2.1\" --new_build \"2.1.0-ER1\" --new_tester \"shadowman@redhat.com\" --new_manager \"shadowman@redhat.com\"")
-    parser.add_option("--plan",         dest="plan",        type="int",     help="test plan id",    default=5709)
-    parser.add_option("--build",        dest="build",       type="string",  help="build name",      default="EAP6.3.0.ER4")
-    parser.add_option("--product",      dest="product",     type="string",  help="product name",    default="JBoss EAP")
-    parser.add_option("--new_product_version",  dest="new_product_version", type="string", help="product version name", default="6.0")
-    parser.add_option("--new_build",    dest="new_build",   type="string",  help="new build name",  default="EAP6.3.0.ER5")
-    parser.add_option("--new_tester",   dest="new_tester",  type="string",  help="new tester",      default="shadowman@redhat.com")
-    parser.add_option("--new_manager",   dest="new_manager",  type="string",  help="new manager",      default="shadowman@redhat.com")
+    parser.add_option("--plan",                dest="plan",                type="int",    help="test plan id",                default=5709)
+    parser.add_option("--build",               dest="build",               type="string", help="build name",                  default="EAP6.3.0.ER4")
+    parser.add_option("--product",             dest="product",             type="string", help="product name",                default="JBoss EAP")
+    parser.add_option("--new_product_version", dest="new_product_version", type="string", help="product version name",        default="6.0")
+    parser.add_option("--new_build",           dest="new_build",           type="string", help="new build name",              default="EAP6.3.0.ER5")
+    parser.add_option("--new_tester",          dest="new_tester",          type="string", help="new tester",                  default="shadowman@redhat.com")
+    parser.add_option("--new_manager",         dest="new_manager",         type="string", help="new manager",                 default="shadowman@redhat.com")
+    parser.add_option("--run_summary_contains",dest="run_summary_contains",type="string", help="run summary contains",        default=None)
+    parser.add_option("--preserve_caseruns",   dest="preserve_caseruns",   type="string",help="preserve old caseruns states",default=False)
 
     options = parser.parse_args()[0]
     print color("Warning: This script may take minutes to complete.", color="lightred", background="black")
@@ -75,12 +100,12 @@ if __name__ == "__main__":
     runs = [["RUN", "Test run ID", "Test run summary", "Status", "Build"]]
     for testrun in testplan_testruns:
         try:
-            processRuns(testrun, options.product, new_product_version, new_build, new_tester, new_manager)
+            processRuns(testrun, options.product, new_product_version, new_build, new_tester, new_manager, options.run_summary_contains, options.preserve_caseruns)
         except xmlrpclib.ProtocolError, err:
             logerror(err)
             for trial in range(0, 5):
                 try:
-                    processRuns(testrun, options.product, new_product_version, new_build, new_tester, new_manager)
+                    processRuns(testrun, options.product, new_product_version, new_build, new_tester, new_manager, options.run_summary_contains, options.preserve_caseruns)
                     break
                 except xmlrpclib.ProtocolError, err:
                     logerror(err)
